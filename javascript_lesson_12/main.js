67c9029dd15b58c515388e34d339d0fc0588a24d
doron persik <persikdoron@gmail.com>
2022-12-06 19:42:35 +0200

    more editing

---
+++ javascript_lesson_12/main.js
@@ -0,0 +1,46 @@
+// console.log('hello')
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <div>
+//         <button>click</button>
+//         <input type="date">
+//     </div>
+//     <a href="https://www.sport5.co.il/">
+//         <img src="https://photo.one.co.il/Image/GG/7,1/1635487.webp?width=700"/>
+//     </a>
+//     <script src="main.js"></script>
+//     <p>im learning <strong>javascript</strong>from october</p>
+//     <span>avvvv</span> <span>eeeee</span>
+// </body>
+// </html>
+
+
+
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>hey everyone</h1>
+//     <img src="https://img.freepik.com/free-photo/toy-bricks-table-with-word-welcome_144627-47504.jpg?t=st=1668965181~exp=1668965781~hmac=b379f7163694d7014e56121987df77e72ca9c0b9e17adc21e74f72ee5242907a"/>
+//     <div>   
+//         <a href="about.html"> 
+//             <input type="button" value="click me"/>
+//         </a>   
+//      </div>
+// </body>
+// </html> --></meta>
